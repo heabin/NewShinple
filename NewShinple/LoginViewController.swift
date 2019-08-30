ab449876c518e549af405af80ffccb45b63d4b59
JoHaebin <heabin209@gmail.com>
2019-08-30 12:53:40 +0900

    first

---
+++ NewShinple/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewShinple
+//
+//  Created by user on 30/08/2019.
+//  Copyright © 2019 veronica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
