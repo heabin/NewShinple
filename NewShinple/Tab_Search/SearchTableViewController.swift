d8f7553e420652a64e339a561aa12dc33a3f0144
JoHaebin <heabin209@gmail.com>
2019-08-31 22:15:15 +0900

    2215

--- NewShinple/Tab_Search/SearchTableViewController.swift
+++ NewShinple/Tab_Search/SearchTableViewController.swift
@@ -13,36 +13,16 @@ import UIKit
  */
 class SearchTableViewController: UITableViewController {
 
+    let sampleData = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"]
+    
+    var searchedData = [String]()
+    var searching = false
+    
     @IBOutlet weak var searchBar: UISearchBar!
     
     
-    let colorLightGray = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+    var baseArray: [String] = ["IC","ss","A","집에 가고싶다","개발개발", "강의 듣기"]
     
-    let colorStartBlue = UIColor(red: 37/255, green: 97/255, blue: 166/255, alpha: 1)
-    let colorMiddleBlue = UIColor(red: 45/255, green: 132/255, blue: 194/255, alpha: 1)
-    let colorEndBlue = UIColor(red: 53/255, green: 169/255, blue: 223/255, alpha: 1)
-    
-    var isSearchBarFocused = false
-    var lectureArray = [String]()
-    var currentLectureArray = [String]()
-    var searchedBefore = [String?]()
-    struct cgFloatInt {
-        var width: Int
-        var height: Int
-        var widthSpacing: Int
-        var heightSpacing: Int
-        var buttonSizing: Int
-        init(w: Int, h:Int, ws:Int, hs:Int, bs:Int) {
-            width = w
-            height = h
-            widthSpacing = ws
-            heightSpacing = hs
-            buttonSizing = bs
-        }
-    }
-    
-    var searchArray: [String] = ["IC","ss","A","집에 가고싶다","개발개발", "강의 듣기"]
-    var custom: cgFloatInt = cgFloatInt(w: 25, h: 30, ws: 200, hs:35, bs:100)
  
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -50,23 +30,20 @@ class SearchTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return searchArray.count+1
+        if searching{
+            return searchedData.count
+        }else {
+            return baseArray.count
+        }
     }
     
     //목록 삭제 함수
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
-        if(indexPath.row >= 2){
-        print("ddd")
-            if(editingStyle == .delete){
-                searchArray.remove(at: (indexPath as NSIndexPath).row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            }else if(editingStyle == .insert){
-                
-            }
+        if(editingStyle == .delete){
+            baseArray.remove(at: (indexPath as NSIndexPath).row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
         }
-        
     }
     
     // 선택 셀 삭제 불가 함수
@@ -74,10 +51,8 @@ class SearchTableViewController: UITableViewController {
         var edit: Bool = true;
         
         if(indexPath.row == 0) {
-            
             edit = false;
         }
-        
         return edit;
     }
     
@@ -88,8 +63,6 @@ class SearchTableViewController: UITableViewController {
         return "삭제"
     }
     
-    
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         var rowline = indexPath.row
@@ -97,83 +70,51 @@ class SearchTableViewController: UITableViewController {
         if(rowline == 0){
             let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell1", for: indexPath) as! SearchTableViewCell1
             
-            cell.label.text = "최근 검색기록 입니다."
+            cell.label.text = "최근 검색기록 입니다.   "
+            
+            cell.allDeleteBtn.addTarget(self, action: #selector(delectCell), for: .touchUpInside)
 //            cell.allDeleteBtn?.textInputContextIdentifier = "X"
             
             return cell
         }else{
             let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell2", for: indexPath) as! SearchTableViewCell2
-            cell.searchContent.text = searchArray[indexPath.row-1]
-            
-            
+            if searching {
+                cell.searchContent.text = searchedData[indexPath.row]
+            } else {
+                cell.searchContent.text = sampleData[indexPath.row]
+            }
             return cell
         }
        
     }
     
-
-
+    //전체 삭제 함수
+    @objc func delectCell() {
+        baseArray = [""]
+        tableView.reloadData()
+    }
+    
     //-------------viewDidLoad-------------
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchBar.delegate = self
         tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
-        
-        
     }
-    
-    
-    // called when text starts editing
-    @available(iOS 2.0, *)
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        isSearchBarFocused = true
-        self.searchBar.showsCancelButton = true
-        if searchedBefore.isEmpty {
-//            self.view.bringSubviewToFront(lblNoPreviousSample)
-        } else {
-//            self.view.bringSubviewToFront(searchedScrollView)
-        }
-        print("searchBarTextDidBeginEditing")
-    }
-    
+}
+
+extension SearchTableViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard !searchText.isEmpty else {
-            if searchedBefore.isEmpty {
-//                self.view.bringSubviewToFront(lblNoPreviousSample)
-            } else {
-//                self.view.bringSubviewToFront(searchedScrollView)
-            }
-            tableView.isHidden = true
-            currentLectureArray = lectureArray
-            tableView.reloadData()
-            return
-        }
-        tableView.isHidden = false
-        self.view.bringSubviewToFront(tableView)
-        currentLectureArray = lectureArray.filter({ lecture -> Bool in
-            lecture.lowercased().contains(searchText.lowercased())
-        })
+        searchedData = sampleData.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+        searching = true
         tableView.reloadData()
     }
     
-    // called when cancel button pressed
-    @available(iOS 2.0, *)
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        isSearchBarFocused = false
+        searching = false
+        searchBar.text = ""
         tableView.reloadData()
-//        self.view.bringSubviewToFront(btnSearchHashtag)
-//        self.view.bringSubviewToFront(btnSearchCategory)
-        if searchedBefore.isEmpty {
-//            self.view.bringSubviewToFront(lblNoPreviousSample)
-        } else {
-//            self.view.bringSubviewToFront(searchedScrollView)
-        }
-        searchBar.endEditing(true)
-        self.searchBar.text = ""
-        self.searchBar.showsCancelButton = false
     }
-    
     
 }
